0e72873737a27e5a331ad59a6e5af3da4ee9fa90
tivi2004 <38327519021@mby.co.il>
2025-03-19 15:27:12 +0200

    19.03.2025 delete user

--- client/src/commponents/User.jsx
+++ client/src/commponents/User.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react"
 import Axios from "axios"
-
+import { Button } from 'primereact/button';
+import { DataScroller } from 'primereact/datascroller';
+import { Rating } from 'primereact/rating';
+import { Tag } from 'primereact/tag';
 
 const Users=()=>{
 
@@ -11,21 +14,58 @@ const getUser = async ()=> {
     setUser(data)
 }
 
+const deleteUser = async (id)=> {
+    const {data} = await Axios.delete(`http://localhost:1233/api/User/${id}`)
+    getUser();
+}
 useEffect(()=>{
     getUser()
 },[])
 
+
+
+const itemTemplate = (user) => {
+    return (
+        <div className="col-12">
+            <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
+                <div className="flex flex-column lg:flex-row justify-content-between align-items-center xl:align-items-start lg:flex-1 gap-4">
+                    <div className="flex flex-column align-items-center lg:align-items-start gap-3">
+                        <div className="flex flex-column gap-1">
+                            <div className="text-2xl font-bold text-900">{user.name}</div>
+                            <div className="text-700">{user._id}</div>
+                        </div>
+                        <div className="flex flex-column gap-2">
+                            <span className="flex align-items-center gap-2">
+                                <i className="pi pi-building"></i>
+                                <span className="font-semibold">{user.address}</span>
+                            </span>
+                        </div>
+                        <div className="flex flex-column gap-2">
+                            <span className="flex align-items-center gap-2">
+                                <i className="pi pi-envelope"></i>
+                                <span className="font-semibold">{user.email}</span>
+                            </span>
+                        </div>
+                        <div className="flex flex-column gap-2">
+                            <span className="flex align-items-center gap-2">
+                                <i className="pi pi-phone"></i>
+                                <span className="font-semibold">{user.phone}</span>
+                            </span>
+                        </div>
+                    </div>
+                    <div className="flex flex-row lg:flex-column align-items-center lg:align-items-end gap-4 lg:gap-2">
+                        <Button icon="pi pi-trash" label="Delet" onClick={()=>{deleteUser(user._id)}} ></Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
 return (
-    <>
-<h1>User</h1>
-{
-  user.map((item)=>{
-return <div>
-    {item.name}
-</div>
-  })  
-}
-    </>
+    <div className="card">
+        <DataScroller value={user} itemTemplate={itemTemplate} rows={5} inline scrollHeight="500px" header="Scroll Down to Load More" />
+    </div>
 )
 }
 
